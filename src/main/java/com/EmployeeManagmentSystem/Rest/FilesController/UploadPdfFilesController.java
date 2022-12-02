686afac9a9b916d2f8bdade3e021411f9717c3a3
krishnanare <86293602+krishnanare@users.noreply.github.com>
2022-12-02 13:46:59 +0530

    New V3 Files UploadationPart

---
+++ src/main/java/com/EmployeeManagmentSystem/Rest/FilesController/UploadPdfFilesController.java
@@ -0,0 +1,34 @@
+package com.EmployeeManagmentSystem.Rest.FilesController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author krishnakumar
+ *  "/Employees/{sapid}/EmployeeInfo/BankDetails"
+ */
+@RestController
+@RequestMapping("api/service/")
+public class UploadPdfFilesController {
+	
+    //   bank details , educationDetails (marksMemo,)  ,  -- bankpassbook.doc ,pdf 
+    //   For personal details -- form or pdf \
+    //  AdharCard -- sapid, name ,sex, adhar_number
+    //  Pan Card -- sapid,PAN number , name 
+	//  resume upload ,profiles , certifications 
+	//  Get - paySlips  based on month  
+     /**
+  	 * @param firstName
+  	 * @param lastName
+  	 * @param middleName
+  	 * @param maritualStatus
+  	 * @param gender
+  	 * @param nationality
+  	 * @param countryOfBirth
+  	 * @param stateOfBirth
+  	 * @param citizen
+  	 * @param dateOfBirth
+  	 * @param familyDetails
+  	 */
+     
+}
